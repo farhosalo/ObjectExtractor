79ed2e06e32aaeea2b06ae7080ce7c162d86f0cf
Salo <info@salo.com.de>
2026-01-03 19:07:52 +0100

    Set OwlVit as default underlying model

--- Configuration.py
+++ Configuration.py
@@ -4,5 +4,5 @@ config = {
     "OUTPUT_PATH": "Output",  # Path to the output directory
     "MINIMUM_HEIGHT": 24,  # Minimum height of the object to be extracted
     "MINIMUM_WIDTH": 24,  # Minimum width of the object to be extracted
-    "UNDERLYING_MODEL": "GroundingDINO",  # Underlying model to use for object extraction. Supported models: "OwlVit", "GroundingDINO"
+    "UNDERLYING_MODEL": "OwlVit",  # Underlying model to use for object extraction. Supported models: "OwlVit", "GroundingDINO"
 }
