2f12286bd576aa9d6de54ec4a1c72ce2c421f470
Salo <info@salo.com.de>
2026-01-03 19:09:37 +0100

    Use timestamp for extracted object's filename

--- ObjectExtractor/AbstractObjectExtractor.py
+++ ObjectExtractor/AbstractObjectExtractor.py
@@ -1,11 +1,9 @@
-import logging.config
 import os
 from abc import ABC, abstractmethod
 import re
 import cv2
 import logging
-
-logging.basicConfig(level=logging.INFO)
+from datetime import datetime
 
 
 class AbstractObjectExtractor(ABC):
@@ -20,7 +18,7 @@ class AbstractObjectExtractor(ABC):
         if not os.path.exists(self.__OutputPath):
             os.makedirs(self.__OutputPath)
 
-        self.__FileIndex = self.__getMaxIndex() + 1
+        self.__FileIndex = 0
 
     def setMinimumObjectSize(self, width, height):
         self._MinimumSignSize = (width, height)
@@ -52,26 +50,10 @@ class AbstractObjectExtractor(ABC):
     def _extractObjectsFromFrame(self):
         pass
 
-    def __getMaxIndex(self):
-        def extract_number(f):
-            s = re.findall("(\d+).jpg", f)
-            return (int(s[0]) if s else -1, f)
-
-        fileList = os.listdir(self.__OutputPath)
-
-        if len(fileList) == 0:
-            return -1
-
-        maxFile = max(fileList, key=extract_number)
-        if maxFile and maxFile.endswith(".jpg"):
-            maxFile = os.path.splitext(maxFile)[0]
-            return int(maxFile.split("_", 1)[1])
-        return -1
-
     def _saveExtractedObject(self, object):
         try:
-            fileName = "Object_%08d.jpg" % (self.__FileIndex)
-            cv2.imwrite(os.path.join(self.__OutputPath, fileName), object)
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S_%f")
+            cv2.imwrite(os.path.join(self.__OutputPath, f"{timestamp}.jpg"), object)
             self.__FileIndex += 1
         except:
             pass
